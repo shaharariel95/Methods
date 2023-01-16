140cd3d80ffa954ceaf078e769d229e6daaadcf3
Shahar <Shaharariel95@gmail.com>
2023-01-16 15:10:05 +0200

    login and register working with hash and mysql

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
 from dotenv import load_dotenv
-from flask import Flask, session
+from flask import Flask
+from flaskext.mysql import MySQL
 from flask_session import Session
+from flask_bcrypt import Bcrypt
+
+import os
 import views
 
 load_dotenv()
 
-
 app = Flask(__name__)
+
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = os.getenv('MYSQL_DATABASE_USER')
+app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_DATABASE_PASSWORD')
+app.config['MYSQL_DATABASE_DB'] = os.getenv('MYSQL_DATABASE_DB')
+app.config['MYSQL_DATABASE_HOST'] = os.getenv('MYSQL_DATABASE_HOST')
+mysql.init_app(app)
+print('connected to db: ' + os.getenv('MYSQL_DATABASE_DB'))
+
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+print('session started')
+
+bcrypt = Bcrypt(app)
 
 app.add_url_rule('/login', methods=['POST'], view_func=views.login)
 app.add_url_rule('/login', methods=['GET'], view_func=views.login_page)
-app.add_url_rule('/register', view_func=views.register)
+app.add_url_rule('/register', methods=['POST'], view_func=views.register)
+app.add_url_rule('/logout', view_func=views.logout)
 app.add_url_rule('/', view_func=views.home)
 
 if __name__ == '__main__':
--- views.py
+++ views.py
@@ -1,8 +1,9 @@
-from flask import render_template, request, session, redirect, g, current_app
+from flask import render_template, request, session, redirect
+import app
 
 
 def home():
-    if not session.get("email"):
+    if not session.get("username"):
         # if not there in the session then redirect to the login page
         return redirect("/login")
     return render_template('home.html')
@@ -13,16 +14,51 @@ def login_page():
 
 
 def login():
-    if request.form['email'] == 'admin@example.com':
-        if request.form['password'] == 'admin':
-            session["email"] = request.form.get("email")
-            print("email: " + session['email'])
-            return redirect('/')
-        else:
-            return login_page()
+    # Get the user input
+    email = request.form['email']
+    password = request.form['password']
+
+    # Create a cursor
+    conn = app.mysql.connect()
+    cursor = conn.cursor()
+
+    # Get the user by username
+    cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
+    columns = [col[0] for col in cursor.description]
+    user = dict(zip(columns, cursor.fetchone()))
+    print(user)
+    # If the user exists and the password is correct
+    if user and app.bcrypt.check_password_hash(user['password'], password):
+        # Create a session for the user
+        session['username'] = user['username']
+        session['email'] = user['email']
+        cursor.close()
+        conn.close()
+        return redirect('/')
     else:
-        return login_page()
+        cursor.close()
+        conn.close()
+        return 'Invalid username/password'
+
+
+def logout():
+    session["email"] = None
+    session["username"] = None
+    return redirect("/")
 
 
 def register():
-    return render_template('register.html')
+    username = request.form['username']
+    email = request.form['email']
+    password = request.form['password']
+    pw_hashed = app.bcrypt.generate_password_hash(password, 10)
+    conn = app.mysql.connect()
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO `users` (`username`, `password`, `email`)"
+                   " VALUES (%s, %s , %s)", (username, pw_hashed, email))
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+    return redirect('/')
